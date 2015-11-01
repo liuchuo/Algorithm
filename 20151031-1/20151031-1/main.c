049adb1401d0a05014d0f8b9e83a05068055a33c
陈欣 <95323362@qq.com>
2015-11-01 23:40:47 +0800

    用枚举填运算符-debug

--- 20151031-1/20151031-1/main.c
+++ 20151031-1/20151031-1/main.c
@@ -10,7 +10,7 @@ int main() {
     float left,right;
     char oper[5] = {' ','+','-','*','/'};
     printf("请输入5个数：");
-    for(j = 1;j < 5;j++)
+    for(j = 1;j <= 5;j++)
         scanf("%d",&num[j]);
     printf("请输入结果：");
     scanf("%d",&result);
@@ -19,14 +19,16 @@ int main() {
             for(i[2] = 1;i[2] <= 4;i[2]++) {
                 if((i[2] < 4) || (num[3] != 0)) {
                     for(i[3] = 1;i[3] <= 4;i[3]++) {
-                        if((i[3] < 4) || (num[5] != 0)) {
+                        if((i[3] < 4) || (num[4] != 0)) {
+                            for(i[4] = 1;i[4] <= 4;i[4]++) {
+                                if((i[4] < 4) || (num[5] != 0)) {
                             left = 0;
                             right = num[1];
                             sign = 1;
                             for(j = 1;j <= 4;j++) {
                                 switch(oper[i[j]]) {
                                     case '+':
-                                        left = left + sign *right;
+                                        left = left + sign * right;
                                         sign = 1;
                                         right = num[j + 1];
                                         break;
@@ -50,6 +52,9 @@ int main() {
                                     printf("%d %c ",num[j],oper[i[j]]);
                                 }
                                 printf("%d = %d\n",num[5],result);
+                            }
+                                    
+                                }
                             }
                         }
                     }
