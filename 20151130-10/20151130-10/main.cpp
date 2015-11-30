a19d8d8a22045a53c037edc32e62d2709752d619
陈欣 <95323362@qq.com>
2015-11-30 21:18:41 +0800

    5.1.5模板

---
+++ 20151130-10/20151130-10/main.cpp
@@ -0,0 +1,35 @@
+//5.1.5模板
+#include <iostream>
+using namespace std;
+
+struct Point {
+    int x, y;
+    Point(int x = 0, int y = 0) : x(x),y(y) {}
+};
+
+Point operator + (const Point &A, const Point &B) {
+    return Point(A.x + B.x, A.y + B.y);
+}
+
+ostream &operator << (ostream &out, const Point &p) {
+    out << "(" << p.x << "," << p.y << ")";
+    return out;
+}
+
+template <typename T>
+T sum(T *begin, T *end) {
+    T *p = begin;
+    T sum = 0;
+    for (T *p = begin; p != end; p++) {
+        sum = sum + *p;
+    }
+    return sum;
+}
+
+int main() {
+    double a[] = {1.1, 2.2, 3.3, 4.4};
+    cout << sum(a, a+4) << endl;
+    Point b[] = {Point(1, 2), Point(3, 4), Point(5, 6), Point(7, 8)};
+    cout << sum(b, b + 4) << endl;
+    return 0;
+}
