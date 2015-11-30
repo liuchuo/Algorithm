d476c070020c1adeb0a9d741e8cf2c6ec09f5119
陈欣 <95323362@qq.com>
2015-11-30 20:19:50 +0800

    #include <algorithm> 里的swap

---
+++ 20151130-8/20151130-8/main.cpp
@@ -0,0 +1,13 @@
+//#include <algorithm> 里的swap
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    double a = 0.9;
+    double b = 1.5;
+    swap(a, b);
+    cout << "a = " << a << endl;
+    cout << "b = " << b << endl;
+    return 0;
+}
