b0799ec46cf22c14e3214746d4b08f545d8b288b
陈欣 <95323362@qq.com>
2016-03-10 09:30:04 +0800

    reverse

---
+++ reverse.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+int main() {
+    int a[10] = {987, 654, 32, 1, 0};
+    reverse(a, a + 10);
+    reverse(begin(a), end(a));
+    for (int i = 0; i < 10; i++) {
+        cout << a[i] << " ";
+    }
+    return 0;
+}
